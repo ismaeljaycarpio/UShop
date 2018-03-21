204e3f4e8fd336c61c38a059d5291b22f6438455
ismael <ismaeljaycarpio1992@gmail.com>
2018-03-21 17:16:57 +0800

    added index and details

---
+++ UShop/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UShop.Models;
+
+namespace UShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public CustomerController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            //base.Dispose(disposing);
+            context.Dispose();
+        }
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var model = context.Customers;
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = context.Customers.SingleOrDefault(c => c.Id == id);
+            if (model == null)
+                return HttpNotFound();
+
+            return View(model);
+        }
+    }
+}
